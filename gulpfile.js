741f20f20bcdd0edc7012ee09aa1f0f9a076738d
mgmeyers <matthew.gregory.meyers@gmail.com>
2021-03-31 10:14:33 -0700

    Prep for style settings plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
+require("dotenv").config();
+
 const gulp = require("gulp");
 const concat = require("gulp-concat");
 const rename = require("gulp-rename");
 
-const config = require("./dev-config");
-
 gulp.task("css", function () {
-  if (config.themeDestination) {
+  if (process.env.THEME_DEST) {
     return gulp
       .src("./src/*.css")
       .pipe(concat("obsidian.css"))
       .pipe(gulp.dest("./"))
       .pipe(rename("California Coast.css"))
-      .pipe(gulp.dest(config.themeDestination));
+      .pipe(gulp.dest(process.env.THEME_DEST));
   } else {
     return gulp
       .src("./src/*.css")
