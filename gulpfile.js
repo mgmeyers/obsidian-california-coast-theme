78aaed541133a76adecf280d75a7bf91c7c14b73
mgmeyers <matthew.gregory.meyers@gmail.com>
2021-11-21 11:41:45 -0800

    Tweaks

--- gulpfile.js
+++ gulpfile.js
@@ -23,3 +23,23 @@ gulp.task("css", function () {
 gulp.task("css:watch", function () {
   gulp.watch("./src/*.css", gulp.series("css"));
 });
+
+gulp.task("css2", function () {
+  if (process.env.THEME_DEST) {
+    return gulp
+      .src("./src.v2/*.css")
+      .pipe(concat("obsidian.css"))
+      .pipe(gulp.dest("./"))
+      .pipe(rename("California Coast.css"))
+      .pipe(gulp.dest(process.env.THEME_DEST2));
+  } else {
+    return gulp
+      .src("./src.v2/*.css")
+      .pipe(concat("obsidian.css"))
+      .pipe(gulp.dest("./"));
+  }
+});
+
+gulp.task("css2:watch", function () {
+  gulp.watch("./src.v2/*.css", gulp.series("css"));
+});
