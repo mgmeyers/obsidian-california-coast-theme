9aabac9082b17d852127e534173515a8c130fe04
mgmeyers <matthew.gregory.meyers@gmail.com>
2021-12-14 18:47:44 -0800

    WIP v2

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const gulp = require("gulp");
 const concat = require("gulp-concat");
 const rename = require("gulp-rename");
+const sass = require('gulp-sass')(require('sass'));
 
 gulp.task("css", function () {
   if (process.env.THEME_DEST) {
@@ -27,19 +28,21 @@ gulp.task("css:watch", function () {
 gulp.task("css2", function () {
   if (process.env.THEME_DEST) {
     return gulp
-      .src("./src.v2/*.css")
-      .pipe(concat("obsidian.css"))
+      .src("./src.v2/**/*.scss")
+      .pipe(concat("obsidian.scss"))
+      .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest("./"))
       .pipe(rename("California Coast.css"))
       .pipe(gulp.dest(process.env.THEME_DEST2));
   } else {
     return gulp
-      .src("./src.v2/*.css")
-      .pipe(concat("obsidian.css"))
+      .src("./src.v2/*.scss")
+      .pipe(concat("obsidian.scss"))
+      .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest("./"));
   }
 });
 
 gulp.task("css2:watch", function () {
-  gulp.watch("./src.v2/*.css", gulp.series("css"));
+  gulp.watch("./src.v2/**/*.scss", gulp.series("css2"));
 });
