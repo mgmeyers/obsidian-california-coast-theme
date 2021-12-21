087000b71584a365850e385daafbc11ac7f0f541
mgmeyers <matthew.gregory.meyers@gmail.com>
2021-12-21 12:54:40 -0800

    v 2.0

--- gulpfile.js
+++ gulpfile.js
@@ -8,41 +8,21 @@ const sass = require('gulp-sass')(require('sass'));
 gulp.task("css", function () {
   if (process.env.THEME_DEST) {
     return gulp
-      .src("./src/*.css")
-      .pipe(concat("obsidian.css"))
+      .src("./src/**/*.scss")
+      .pipe(concat("obsidian.scss"))
+      .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest("./"))
       .pipe(rename("California Coast.css"))
       .pipe(gulp.dest(process.env.THEME_DEST));
   } else {
     return gulp
-      .src("./src/*.css")
-      .pipe(concat("obsidian.css"))
+      .src("./src/*.scss")
+      .pipe(concat("obsidian.scss"))
+      .pipe(sass().on('error', sass.logError))
       .pipe(gulp.dest("./"));
   }
 });
 
 gulp.task("css:watch", function () {
-  gulp.watch("./src/*.css", gulp.series("css"));
-});
-
-gulp.task("css2", function () {
-  if (process.env.THEME_DEST) {
-    return gulp
-      .src("./src.v2/**/*.scss")
-      .pipe(concat("obsidian.scss"))
-      .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest("./"))
-      .pipe(rename("California Coast.css"))
-      .pipe(gulp.dest(process.env.THEME_DEST2));
-  } else {
-    return gulp
-      .src("./src.v2/*.scss")
-      .pipe(concat("obsidian.scss"))
-      .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest("./"));
-  }
-});
-
-gulp.task("css2:watch", function () {
-  gulp.watch("./src.v2/**/*.scss", gulp.series("css2"));
+  gulp.watch("./src/**/*.scss", gulp.series("css"));
 });
